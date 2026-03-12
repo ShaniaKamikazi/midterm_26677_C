a56023e5735670eb20cd4528e8250518cbc5cc9e
ShaniaKamikazi <kamikazishania@gmail.com>
2026-03-12 08:45:00 +0200

    added exception classes for error handling

---
+++ src/main/java/auca/ac/rw/FinanceTracker/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package auca.ac.rw.FinanceTracker.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/auca/ac/rw/FinanceTracker/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package auca.ac.rw.FinanceTracker.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
